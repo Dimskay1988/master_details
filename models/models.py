4dddaf94d6aedf0b07b8d15f20e09aa758381fe1
anikeenko <dimskay1988@icloud.com>
2023-07-06 21:02:55 +0300

    EDIT module

--- models/models.py
+++ models/models.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 
-class MasterDetails(models.Model):
-    _name = 'masterdetails.masterdetails'
-    _description = 'Master Details'
+class Contract(models.Model):
+    _name = 'contract.contract'
+    _description = 'Contracts'
 
-    name = fields.Char(string='Name')
-    value = fields.Integer(string='Value')
-    value2 = fields.Float(string='Value 2')
+    name = fields.Char(string='Contract Number')
+    description = fields.Text(string='Description')
+    acts_ids = fields.One2many('contract.act', 'contract_id', string='Acts')
 
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+
+class Act(models.Model):
+    _name = 'contract.act'
+    _description = 'Acts'
+
+    name = fields.Char(string='Act Number')
+    description = fields.Text(string='Description')
+    contract_id = fields.Many2one('contract.contract', string='Contract')
